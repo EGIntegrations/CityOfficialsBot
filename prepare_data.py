9d6f66ecaf611c5cbee23de552e87bed12414495
EGOD21 <140920088+EGOD21@users.noreply.github.com>
2025-04-01 09:25:01 -0500

    Delete prepare_data.py

--- prepare_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('ordinances.csv')
-with open("ordinances.txt", "w", encoding="utf-8") as file:
-    for _, row in df.iterrows():
-        file.write(f"{row['title']}\n{row['content']}\n\n{'-'*80}\n\n")
-
-print("Data prepared successfully!")
